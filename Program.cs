24e0daf0a65285bc9a131e3f786d437b618fe5cd
ryhanearefi <r_arefy@yahoo.com>
2023-06-22 17:49:57 +0330

    message

--- Program.cs
+++ Program.cs
@@ -1,21 +1,7 @@
-﻿internal class Program
+﻿public class Parentcar
 {
-  private static void Main(string[] args)
-  {
-    // Read five nouns from the input
-    Console.WriteLine("Please enter 5 names:");
-    string[] names = new string[5];
-    for (int i = 0; i < 5; i++)
-    {
-      Console.Write("{0}: ", i + 1);
-      names[i] = Console.ReadLine();
-    }
+  //name
+  
 
-    Array.Sort(names);
-    Console.WriteLine("name moratab shode:");
-    foreach (string name in names)
-    {
-      Console.WriteLine(name);
-    }
-  }
+
 }
